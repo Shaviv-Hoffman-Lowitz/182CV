3eba87bb3c8671d1542dae41b2bf570b8643eeda
Shaviv <shaviv@berkeley.edu>
2021-04-29 15:17:27 -0700

    Working on creating 5 different adversarial datasets and then ensembling the models together

--- train_sample_torch.py
+++ train_sample_torch.py
@@ -14,6 +14,9 @@ import torchvision
 import torchvision.transforms as transforms
 import time
 
+from art.estimators.classification import PyTorchClassifier
+from art.attacks.evasion import SpatialTransformation, DeepFool, SquareAttack, FastGradientMethod, BasicIterativeMethod
+
 from model import Net
 from torchvision import models
 from torch import nn
@@ -42,8 +45,18 @@ def main(args):
     ])
     train_set = torchvision.datasets.ImageFolder(
         data_dir / 'train', data_transforms)
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
-                                               shuffle=True, num_workers=4, pin_memory=True)
+
+    # Changed it so that the batch size is len(train_set) to try to get training_data and training_labels to be of the right shapes
+    # so that I can pass them into the fit function later on
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=len(train_set),
+                                                shuffle=True, num_workers=4, pin_memory=True)
+
+    # Need to figure out how to speed this up
+    training_data = next(iter(train_loader))[0].numpy()
+
+    # Can be one-hot encoded or not, either way is totally fine
+    # Also needs to be sped up
+    training_labels = next(iter(train_loader))[1].numpy()
 
     # Creating a model
     model = models.resnext101_32x8d(pretrained=True)
@@ -70,34 +83,88 @@ def main(args):
     optim = torch.optim.Adam(model.parameters())
 
     criterion = nn.CrossEntropyLoss().to(device)
-    for i in range(num_epochs):
-        train_total, train_correct = 0, 0
-        start_time = time.time()
-        for idx, (inputs, targets) in enumerate(train_loader):
-            inputs = inputs.to(device)
-            targets = targets.to(device)
-            optim.zero_grad()
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-            loss.backward()
-            optim.step()
-            _, predicted = outputs.max(1)
-            train_total += targets.size(0)
-            train_correct += predicted.eq(targets).sum().item()
-            # print("\r", end='')
-            if idx % args.freq == 0:
-                print(f"""
-                training {100 * idx / len(train_loader):.2f}%: {train_correct / train_total:.3f}\n
-                Exe Time Per Image: {(time.time()-start_time)/((1+idx)*args.B)} s
-                """)
-        # torch.save({
-        #     'net': model.state_dict(),
-        # }, 'latest.pt')
-        save_checkpoint({
-            'epoch': i+1,
-            'net': model.state_dict(),
-            'acc': 1.*train_correct/train_total,
-        }, 'latest.pt')
+
+    # Not sure if we should train the initial classifier on the training data before we use the initial_classifier to generate adversarial datasets
+    # Definitely should experiment with both ways if we're not sure
+    initial_classifier = PyTorchClassifier(model = model, optimizer = optim, loss = criterion, nb_classes = len(CLASS_NAMES), input_shape = (3, im_height, im_width), device_type = 'gpu')
+    initial_classifier.fit(training_data, training_labels, nb_epochs = num_epochs, batch_size = batch_size)
+
+    square_attack = SquareAttack(initial_classifier)
+
+    spatial_transformation_attack = SpatialTransformation(initial_classifier, 20, 1, 30, 1)
+
+    deepfool_attack = DeepFool(initial_classifier)
+
+    basic_iterative_method_attack = BasicIterativeMethod(initial_classifier)
+
+    fast_gradient_method_attack = FastGradientMethod(initial_classifier)
+
+    # training_labels do not need to be passed in, it is optional - should experiment with both ways
+    square_attack_adversarial_dataset = square_attack.generate(training_data)
+
+    # Labels are not optional for this attack, its not totally clear whether training_labels are supposed be one-hot encoded or not
+    deepfool_adversarial_dataset = deepfool_attack.generate(training_data, training_labels)
+
+    # Labels are not optional for this attack, its not totally clear whether training_labels are supposed be one-hot encoded or not
+    spatial_transformation_adversarial_dataset = spatial_transformation_attack.generate(training_data, training_labels)
+
+    # training_labels do not need to be passed in, it is optional - should experiment with both ways
+    fast_gradient_method_adversarial_dataset = fast_gradient_method_attack.generate(training_data)
+
+    # training_labels do not need to be passed in, it is optional - should experiment with both ways
+    basic_iterative_method_adversarial_dataset = basic_iterative_method_attack.generate(training_data)
+
+    #Creating a list of the adversarial datasets
+    adversarial_datasets = [square_attack_adversarial_dataset, deepfool_adversarial_dataset, spatial_transformation_adversarial_dataset, fast_gradient_method_adversarial_dataset, basic_iterative_method_adversarial_dataset]
+
+    # Generating 5 pretrained models, where only the last layer is not frozen - I think this is necessary, right?
+    pretrained_models = []
+    for num_iterations in range(5):
+        model = models.resnext101_32x8d(pretrained=True)
+
+        # Freezing the weights from the pretrained model
+        for param in model.parameters():
+            param.requires_grad = False
+
+        # Creating a final fully connected layer that will be trained in the training loop
+        number_of_features = model.fc.in_features
+
+        model.fc = nn.Linear(number_of_features, len(CLASS_NAMES))
+        model.to(device)
+
+        # We should experiment with other optimizers as well
+        optim = torch.optim.Adam(model.parameters())
+        criterion = nn.CrossEntropyLoss().to(device)
+
+        current_classifier = PyTorchClassifier(model = model, optimizer = optim, loss = criterion, nb_classes = len(CLASS_NAMES), input_shape = (3, im_height, im_width), device_type = 'gpu')
+        pretrained_models.append(current_classifier)
+
+    # Training the 5 pretrained models, each on a different adversarial dataset
+    for index in range(len(pretrained_models)):
+        model = pretrained_models[index]
+        adversarial_dataset = adversarial_datasets[index]
+
+        # Labels should be the same as they were originally, right?
+        model.fit(adversarial_dataset, training_labels, nb_epochs = num_epochs, batch_size = batch_size)
+
+    # Now we need to ensemble the models together (should we also include the initial_classifier, which was trained on normal, non-adversarial data, in our ensemble?)
+
+
+
+
+
+
+
+
+    # torch.save({
+    #     'net': model.state_dict(),
+    # }, 'latest.pt')
+    
+    #save_checkpoint({
+        #'epoch': i+1,
+        #'net': model.state_dict(),
+        #'acc': 1.*train_correct/train_total,
+    #}, 'latest.pt')
 
 
 best_acc = 0
