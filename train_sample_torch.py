a63749aea1073b907c79c1cc0ead8053c3f82988
Eashaan Katiyar <eashaank@gmail.com>
2021-05-05 23:53:26 -0700

    Updates to training

--- train_sample_torch.py
+++ train_sample_torch.py
@@ -13,10 +13,12 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import time
+import os
 
-from model import Net
-from torchvision import models
+import pretrainedmodels as models
 from torch import nn
+from ignite.metrics import Accuracy, TopKCategoricalAccuracy  # , Precision, Recall
+from common import AverageMeter
 
 
 def main(args):
@@ -32,6 +34,7 @@ def main(args):
     im_height = args.H
     im_width = args.W
     num_epochs = args.E
+    start_epoch = 0
 
     data_transforms = transforms.Compose([
         transforms.Resize((im_height, im_width)),
@@ -46,68 +49,101 @@ def main(args):
                                                shuffle=True, num_workers=4, pin_memory=True)
 
     # Creating a model
-    model = models.resnext101_32x8d(pretrained=True)
-
-    # Other models that I experimented with
-    #model = models.alexnet(pretrained=True).cuda()
-    #model = models.resnet101(pretrained=True).cuda()
-
-    # model.eval()
+    model = models.__dict__[args.model](
+        num_classes=1000, pretrained='imagenet')
 
     # Freezing the weights from the pretrained model
     for param in model.parameters():
         param.requires_grad = False
 
     # Creating a final fully connected layer that will be trained in the training loop
-    number_of_features = model.fc.in_features
-    # model.fc = nn.Linear(number_of_features, 200)
+    number_of_features = model.last_linear.in_features
+    model.last_linear = nn.Linear(number_of_features, len(CLASS_NAMES))
 
-    # Could alternatively have used len(CLASS_NAMES), instead of 200, like I did below
-    model.fc = nn.Linear(number_of_features, len(CLASS_NAMES))
-    model.to(device)
+    # This should speed up training
+    model = torch.nn.DataParallel(model).to(device)
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        assert os.path.isfile(
+            args.resume), "=> no checkpoint found at '{}'".format(args.resume)
+        print("=> loading checkpoint '{}'".format(args.resume))
+        checkpoint = torch.load(args.resume)
+        start_epoch = checkpoint['epoch']
+        best_loss = checkpoint['loss']
+        model.load_state_dict(checkpoint['net'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+              .format(args.resume, checkpoint['epoch']))
+
+    torch.backends.cudnn.benchmark = True
 
     # We should experiment with other optimizers as well
-    optim = torch.optim.Adam(model.parameters())
+    optim = torch.optim.Adam(model.parameters(), lr=args.lr)
 
     criterion = nn.CrossEntropyLoss().to(device)
-    for i in range(num_epochs):
-        train_total, train_correct = 0, 0
+
+    # Scheduler reduces lr after 5 epochs without loss reduction in validation
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optim, 'min', patience=5)
+
+    for i in range(start_epoch, num_epochs):
+        for param_group in optim.param_groups:
+            print('Epoch [{}] Learning rate: {}'.format(
+                i, param_group['lr']))
+
+        acc = Accuracy()
+        top5 = TopKCategoricalAccuracy()
+        avg_loss = AverageMeter()
+
         start_time = time.time()
         for idx, (inputs, targets) in enumerate(train_loader):
+            # Load x, y
             inputs = inputs.to(device)
             targets = targets.to(device)
+
+            # Execute
             optim.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, targets)
+
+            # Update Statistics
+            avg_loss.update(loss.item(), inputs.size(0))
+            acc.update((outputs, targets))
+            top5.update((outputs, targets))
+
+            # Backprop and step
             loss.backward()
             optim.step()
-            _, predicted = outputs.max(1)
-            train_total += targets.size(0)
-            train_correct += predicted.eq(targets).sum().item()
-            # print("\r", end='')
+
             if idx % args.freq == 0:
                 print(f"""
-                training {100 * idx / len(train_loader):.2f}%: {train_correct / train_total:.3f}\n
-                Exe Time Per Image: {(time.time()-start_time)/((1+idx)*args.B)} s
+                Training {100 * idx / len(train_loader):.2f}%: Top1: {acc.compute()*100:.2f} \t Top5: {top5.compute()*100:.2f}\n
+                Loss: {avg_loss.val:.4f} ~ {avg_loss.avg:.4f} \n
+                Exe Time Per Image: {(time.time()-start_time)/((1+idx)*args.B)}s
                 """)
-        # torch.save({
-        #     'net': model.state_dict(),
-        # }, 'latest.pt')
+
         save_checkpoint({
             'epoch': i+1,
             'net': model.state_dict(),
-            'acc': 1.*train_correct/train_total,
+            'acc': acc.compute()*100,
+            'top5': top5.compute()*100,
+            'loss': avg_loss.avg
         }, 'latest.pt')
 
+        scheduler.step(avg_loss.avg)
 
-best_acc = 0
+
+best_loss = 0
 
 
 def save_checkpoint(metadata, filename):
-    global best_acc
+    global best_loss
     torch.save(metadata, filename)
-    if metadata['acc'] > best_acc:
-        best_acc = metadata['acc']
+    if metadata['loss'] < best_loss:
+        print(
+            f"""New best model with acc {metadata['acc']} / top5 {metadata['top5']}\n
+            loss red {metadata['loss'] - best_loss}""")
+        best_loss = metadata['loss']
         shutil.copyfile(filename, 'best.pt')
 
 
@@ -123,10 +159,12 @@ if __name__ == '__main__':
     parser.add_argument("-H", help="image height", default=64, type=int)
     parser.add_argument("-W", help="image width", default=64, type=int)
     parser.add_argument("-E", help="num epochs", default=10, type=int)
-    parser.add_argument("-lr", help="learning rate", default=0.1, type=float)
-    parser.add_argument("-m", help='momentum', default=0.9, type=float)
-    parser.add_argument("-wd", help="weight decay", default=1e-4, type=float)
+    parser.add_argument("-lr", help="learning rate", default=0.001, type=float)
     parser.add_argument(
         "-freq", help="print frequency, in batches", default=10, type=int)
+    parser.add_argument("-model", help="model name",
+                        default="inceptionresnetv2", type=str)
+    parser.add_argument(
+        "-resume", help="path to target model", default='', type=str)
     args = parser.parse_args()
     main(args)
