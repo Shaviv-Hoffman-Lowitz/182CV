28d4ee1dfd093a23df266108f3e5a32e13a8d75b
Shaviv <shaviv@berkeley.edu>
2021-05-08 14:19:35 -0700

    Added test_submission_torch.py code so it will work when they run our model

--- test_submission_torch.py
+++ test_submission_torch.py
@@ -1,28 +1,55 @@
-import argparse
 import sys
 import pathlib
 from PIL import Image
 import numpy as np
 import torch
 import torchvision.transforms as transforms
-from EvalDataset import EvalDataset
-from model import Net
-from torchvision import models
-from torch import nn
-from common import AverageMeter, accuracy
+#from model import Net
+
+import pretrainedmodels as models
 
 
-def exportCSV():
+def main():
+    # Load the classes
+    data_dir = pathlib.Path('./data/tiny-imagenet-200/train/')
+    CLASSES = sorted([item.name for item in data_dir.glob('*')])
+
+    # I changed these from 64 to 299
+    im_height, im_width = 299, 299
+
+    ckpt = torch.load('best.pt')
+    
+    # Just deleting the 'pretrained' parameter is all that we would need to do, right?
+    model = models.__dict__["inceptionresnetv2"](num_classes=1000)
+
+    # Creating a final fully connected layer
+    number_of_features = model.last_linear.in_features
+
+    # Changed it to be CLASSES instead of CLASS_NAMES because the variable was named CLASSES in this file
+    model.last_linear = nn.Linear(number_of_features, len(CLASSES))
+
+    model.load_state_dict(ckpt['net'])
+
+    # I don't think we need the line below, do we? They didn't have it in the provided skeleton file
+    # model.to(device)
+
+    model.eval()
+
+    data_transforms = transforms.Compose([
+        transforms.Resize((im_height, im_width)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                             0.229, 0.224, 0.225]),
+    ])
+
     # Loop through the CSV file and make a prediction for each line
-    # Open the evaluation CSV file for writing
-    with open('eval_classified.csv', 'w') as eval_output_file:
-        # Open the input CSV file for reading
-        for line in pathlib.Path(sys.argv[1]).open():
+    with open('eval_classified.csv', 'w') as eval_output_file:  # Open the evaluation CSV file for writing
+        for line in pathlib.Path(sys.argv[1]).open():  # Open the input CSV file for reading
             image_id, image_path, image_height, image_width, image_channels = line.strip().split(
                 ',')  # Extract CSV info
 
-            print(image_id, image_path, image_height,
-                  image_width, image_channels)
+            # They had the print statement below in their skeleton code, should we keep it in or not?
+            # print(image_id, image_path, image_height, image_width, image_channels)
             with open(image_path, 'rb') as f:
                 img = Image.open(f).convert('RGB')
             img = data_transforms(img)[None, :]
@@ -30,77 +57,8 @@ def exportCSV():
             _, predicted = outputs.max(1)
 
             # Write the prediction to the output file
-            eval_output_file.write('{},{}\n'.format(
-                image_id, CLASSES[predicted]))
-
-
-def main(args):
-    # Load the classes
-    data_dir = pathlib.Path('./data/tiny-imagenet-200/train/')
-    CLASSES = sorted([item.name for item in data_dir.glob('*')])
-    im_height, im_width = 64, 64
-
-    ckpt = torch.load('best.pt')
-    print("Loading Model Epoch", ckpt['epoch'], "with train acc", ckpt['acc'])
-
-    # Creating a model
-    model = models.resnext101_32x8d(pretrained=False)
-    number_of_features = model.fc.in_features
-    model.fc = nn.Linear(number_of_features, len(CLASSES))
-
-    model.load_state_dict(ckpt['net'])
-    model.to(device)
-    model.eval()
-
-    data_transforms = transforms.Compose([
-        transforms.Resize((args.H, args.W)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
-                             0.229, 0.224, 0.225]),
-    ])
-
-    with torch.no_grad():
-        if args.CSV:
-            exportCSV()
-        else:
-
-            data_dir = pathlib.Path('./data/tiny-imagenet-200')
-            eval_set = EvalDataset(
-                data_dir / 'val', data_transforms, CLASSES, 'val_annotations.txt')
-            eval_loader = torch.utils.data.DataLoader(eval_set, batch_size=args.B,
-                                                      shuffle=True, num_workers=4, pin_memory=True)
-
-            top1 = AverageMeter()
-            top5 = AverageMeter()
-            for idx, (inputs, targets) in enumerate(eval_loader):
-                inputs = inputs.to(device)
-                targets = targets.to(device)
-                outputs = model(inputs)
-                prec1, prec5 = accuracy(outputs, targets, topk=(1, 5))
-                top1.update(prec1.item(), inputs.size(0))
-                top5.update(prec5.item(), inputs.size(0))
-                if idx % args.freq == 0:
-                    print(
-                        f'Evaluating {100 * idx / len(eval_loader):.2f}%')
-            print(
-                f"Accuracy on Val Set:\n\tTop 1: {top1.avg:.2f}%\n\tTop 5: {top5.avg:.2f}%")
+            eval_output_file.write('{},{}\n'.format(image_id, CLASSES[predicted]))
 
 
 if __name__ == '__main__':
-    print("Using GPU:", torch.cuda.is_available())
-    if torch.cuda.is_available():
-        dev = "cuda:0"
-    else:
-        dev = "cpu"
-    device = torch.device(dev)
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '-CSV', dest='CSV', help='Store Evaluation in CSV file', action='store_true')
-    parser.add_argument("-B", help="batch size", default=32, type=int)
-    parser.add_argument("-H", help="image height", default=64, type=int)
-    parser.add_argument("-W", help="image width", default=64, type=int)
-    parser.add_argument(
-        "-freq", help="print frequency, in batches", default=10, type=int)
-    parser.set_defaults(CSV=False)
-    args = parser.parse_args()
-    main(args)
+    main()
